94a5ac2e5487988eb77e90d13cf1dea587c15699
Xaiver71 <malcolmgray10@gmail.com>
2023-03-15 23:56:19 -0400

    Finished setting up test cases

--- lsp_spring_2023/src/org/howard/edu/lsp/assignment5/IntegerSet.java
+++ lsp_spring_2023/src/org/howard/edu/lsp/assignment5/IntegerSet.java
@@ -6,4 +6,19 @@ package org.howard.edu.lsp.assignment5;
 public class IntegerSet {
 	private List<Integer> set = new ArrayList<Integer>();
 	
+	public void clear() {…};
+	
+	public int length() {…};
+	
+	public boolean equals(IntegerSet b) {…}; 
+	
+	public boolean contains(int value) {…}; 
+	
+	public int largest() throws IntegerSetException {…}; 
+	
+	public int smallest() throws IntegerSetException;
+	
+ 	public void add(int item) {…};
+ 	
+ 	public void union(IntegerSet intSetb) {…};
 }
